00a2147cac22d77374352f2d1297b044c5266588
Rodrigo Junqueira <eurodrigojunqueira@gmail.com>
2024-08-19 21:13:57 -0300

    funções

--- funcoes/exercicio.js
+++ funcoes/exercicio.js
@@ -1,1 +1,83 @@
 console.log("Script de Exercicios:");
+
+// Exercício 1
+// Crie uma função para verificar se um valor é Truthy.
+
+console.log("Exercício 1: ");
+function isTruthy(valor) {
+  return !!valor;
+}
+
+console.log(isTruthy(10));
+
+// Exercício 2
+// Crie uma função matemática que retorne o perímetro de um quadrado.
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+
+console.log("Exercício 2: ");
+function perimetroQuadrado(lado) {
+  return lado * 4;
+}
+
+console.log(perimetroQuadrado(5));
+
+// Exercício 3
+// Crie uma função que retorne o seu nome completo, ela deve possuir os parâmetros: nome e sobrenome
+console.log("Exercício 3: ");
+function nomeCompleto(nome, sobrenome) {
+  return `${nome} ${sobrenome}`;
+}
+console.log(nomeCompleto("Rodrigo", "Junqueira"));
+
+// Exercício 4
+// Crie uma função que verifica se um número é par
+console.log("Exercício 4: ");
+function isEven(numero) {
+  if (numero % 2 == 0) {
+    console.log("É Par");
+  } else {
+    console.log("É ímpar");
+  }
+}
+
+isEven(5);
+
+// Exercício 5
+// Crie uma função que retorne o tipo de dado do argumento passado nela (typeof)
+console.log("Exercício 5: ");
+
+function tipoDeDado(dado) {
+  return typeof dado;
+}
+console.log(tipoDeDado(23));
+
+// Exercício 6
+// addEventListener é uma função nativa do JavaScript o primeiro parâmetro é o evento que ocorre e o segundo o Callback utilize essa função para mostrar no console o seu nome completo quando o evento 'scroll' ocorrer.
+console.log("Exercício 6: ");
+
+addEventListener("scroll", function () {
+  console.log("Rodrigo Junqueira");
+});
+
+// Exercício 7
+// Corrija o erro abaixo
+// function precisoVisitar(paisesVisitados) {
+//   var totalPaises = 193;
+//   return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+// }
+// function jaVisitei(paisesVisitados) {
+//   return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+// }
+// precisoVisitar(20);
+// jaVisitei(20);
+
+function precisoVisitar(paisesVisitados) {
+  var totalPaises = 193;
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+function jaVisitei(paisesVisitados) {
+  var totalPaises = 193;
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+console.log(precisoVisitar(20));
+console.log(jaVisitei(20));
--- funcoes/script.js
+++ funcoes/script.js
@@ -1,1 +1,102 @@
-// Funções 1:
+// Funções:
+
+// Área do quadrado:
+function areaQuadrado(lado) {
+  return lado * lado;
+}
+
+console.log(areaQuadrado(4));
+console.log(areaQuadrado(5));
+
+// --//--
+
+// PI:
+function pi() {
+  return 3.14;
+}
+
+console.log(pi());
+
+// --//--
+
+/* IMC: 
+Peso e altura são os parâmetros */
+function imc(peso, altura) {
+  const imc = peso / altura ** 2;
+  return imc;
+}
+
+console.log(imc(80, 1.8)); // 80 e 1.80 são os argumentos
+console.log(imc(60, 1.7)); // 60 e 1.70 são os argumentos
+
+// --//--
+
+// Cor favorita:
+function corFavorita(cor) {
+  if (cor === "azul") {
+    return "Você gosta do céu";
+  } else if (cor === "verde") {
+    return "Você gosta de mato";
+  } else {
+    return "Você não gosta de nada";
+  }
+}
+
+console.log(corFavorita("verde")); // retorna 'Você não gosta de nada'
+
+// --//--
+
+// Argumentos podem ser funções:
+// Callback -> funções que ocorrem após algum evento
+addEventListener("click", function () {
+  console.log("Clicou");
+});
+// A função possui dois argumentos
+// Primeiro é a string 'click'
+// Segundo é uma função anônima
+
+// --//--
+
+function imc2(peso, altura) {
+  const imc = peso / altura ** 2;
+  console.log(imc);
+}
+
+imc2(80, 1.8); // retorna o imc
+console.log(imc2(80, 1.8)); // retorna o imc e undefined
+
+// --//--
+
+// Verifica idade:
+function terceiraIdade(idade) {
+  if (typeof idade !== "number") {
+    return "Informe a sua idade!";
+  } else if (idade >= 60) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Cuidado, retornar diferentes tipos de dados na mesma função não é uma boa ideia.
+console.log(terceiraIdade(70));
+
+// --//--
+
+//Escopo léxico
+var profissao = "Front-End Developer";
+
+function dados() {
+  var nome = "Rodrigo";
+  var idade = 30;
+  function outrosDados() {
+    var endereco = "Tatuí";
+    var idade = 31;
+    return `${nome}, ${idade}, ${endereco}, ${profissao}`;
+  }
+  return outrosDados();
+}
+
+console.log(dados()); // Retorna 'Rodrigo, 31, Tatuí, Front-End Developer'
+// outrosDados(); // retorna um erro
+
+// --//--
